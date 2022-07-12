bf131d184b125a3a518422889c275908c0c5c9ab
Manh Vu <manhdv2103@gmail.com>
2022-07-12 22:41:13 +0700

    group yanacrontab lines by their type

--- yanacron.hs
+++ yanacron.hs
@@ -4,12 +4,36 @@ import System.IO.Error
 import Control.Exception
 import Data.Text (replace, pack, unpack)
 import Data.List
+import Text.Regex.Posix
+
+type Envs = [String]
+type Jobs = [String]
+type PeriodJobs = [String]
+data LineType = EMPTY | ENV | JOB | PERIOD_JOB | OTHER deriving (Eq)
 
 yanacrontab :: FilePath
 yanacrontab = "./yanacrontab"
 
 mergeTabLines :: String -> [String]
 mergeTabLines l = lines $ unpack $ replace (pack "\\\n") (pack "") (pack l)
+
+classifyLine :: String -> LineType
+classifyLine l
+    | (l =~ "^[ \t]*($|#)") == True = EMPTY
+    | (l =~ "^[ \t]*([^ \t=]+)[ \t]*=(.*)$") == True = ENV
+    | (l =~ "^[ \t]*([[:digit:]]+)[ \t]+([[:digit:]]+)[ \t]+([^ \t/]+)[ \t]+([^ \t].*)$") == True = JOB
+    | (l =~ "^[ \t]*(@[^ \t]+)[ \t]+([[:digit:]]+)[ \t]+([^ \t/]+)[ \t]+([^ \t].*)$") == True = PERIOD_JOB
+    | otherwise = OTHER
+
+groupTabLines :: [String] -> (Envs, Jobs, PeriodJobs)
+groupTabLines = foldl' pushLine ([], [], [])
+    where 
+    pushLine (e, j, pj) l
+        | lineClass == ENV = (e ++ [l], j, pj)
+        | lineClass == JOB = (e, j ++ [l], pj)
+        | lineClass == PERIOD_JOB = (e, j, pj ++ [l])
+        | otherwise = (e, j, pj)
+        where lineClass = classifyLine l
 
 main = toTry `catch` handler
 
@@ -18,7 +42,7 @@ toTry = do
     args <- getArgs
     if length args == 0 then do
         contents <- readFile yanacrontab
-        mapM_ print $ mergeTabLines contents
+        print $ groupTabLines $ mergeTabLines contents
     else return ()
 
 handler :: IOError -> IO ()
