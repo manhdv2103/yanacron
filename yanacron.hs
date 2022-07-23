924e5f580f29945ec568181199dfd6e4a323b509
Manh Vu <manhdv2103@gmail.com>
2022-07-23 17:19:08 +0700

    run jobs in parallel

--- yanacron.hs
+++ yanacron.hs
@@ -1,39 +1,63 @@
+import Control.Concurrent (forkIO, threadDelay)
+import Control.Exception
+import Data.List
+import Data.Text (replace, pack, unpack)
+import Text.Regex.Posix
 import System.Environment
 import System.IO     
 import System.IO.Error
-import Control.Exception
-import Data.Text (replace, pack, unpack)
-import Data.List
-import Text.Regex.Posix
+import System.Process
 
-type Envs = [String]
-type Jobs = [String]
-type PeriodJobs = [String]
-data LineType = EMPTY | ENV | JOB | PERIOD_JOB | OTHER deriving (Eq)
+-- Data & Type
+data Env = Env { envVar :: String
+                , value :: String
+                } deriving (Eq, Show, Read)
+data Job = Job { jPeriods :: Int
+                , jDelays :: Int
+                , jIdent :: String
+                , jCommand :: String
+                } deriving (Eq, Show, Read)
+data PeriodJob = PeriodJob { pjPeriods :: String
+                , pjDelays :: Int
+                , pjIdent :: String
+                , pjCommand :: String
+                } deriving (Eq, Show, Read)
 
+type Envs = [Env]
+type Jobs = [Job]
+type PeriodJobs = [PeriodJob]
+
+-- File and directory
 yanacrontab :: FilePath
 yanacrontab = "./yanacrontab"
 
+spool :: FilePath
+spool = "./spool/yanacron/"
+
+-- Data making functions
+mkEnv :: [String] -> Env
+mkEnv xs = Env {envVar=xs !! 0, value=xs !! 1}
+
+mkJob :: [String] -> Job
+mkJob xs = Job {jPeriods=read (xs !! 0), jDelays=read (xs !! 1), jIdent=xs !! 2, jCommand=xs !! 3}
+
+mkPeriodJob :: [String] -> PeriodJob
+mkPeriodJob xs = PeriodJob {pjPeriods=xs !! 0, pjDelays=read (xs !! 1), pjIdent=xs !! 2, pjCommand=xs !! 3}
+
+-- Tab line manipulation functions
 mergeTabLines :: String -> [String]
 mergeTabLines l = lines $ unpack $ replace (pack "\\\n") (pack "") (pack l)
 
-classifyLine :: String -> LineType
-classifyLine l
-    | (l =~ "^[ \t]*($|#)") == True = EMPTY
-    | (l =~ "^[ \t]*([^ \t=]+)[ \t]*=(.*)$") == True = ENV
-    | (l =~ "^[ \t]*([[:digit:]]+)[ \t]+([[:digit:]]+)[ \t]+([^ \t/]+)[ \t]+([^ \t].*)$") == True = JOB
-    | (l =~ "^[ \t]*(@[^ \t]+)[ \t]+([[:digit:]]+)[ \t]+([^ \t/]+)[ \t]+([^ \t].*)$") == True = PERIOD_JOB
-    | otherwise = OTHER
-
-groupTabLines :: [String] -> (Envs, Jobs, PeriodJobs)
-groupTabLines = foldl' pushLine ([], [], [])
+parseTabLines :: [String] -> (Envs, Jobs, PeriodJobs)
+parseTabLines = foldl' pushLine ([], [], [])
     where 
+    pushLine :: (Envs, Jobs, PeriodJobs) -> String -> (Envs, Jobs, PeriodJobs)
     pushLine (e, j, pj) l
-        | lineClass == ENV = (e ++ [l], j, pj)
-        | lineClass == JOB = (e, j ++ [l], pj)
-        | lineClass == PERIOD_JOB = (e, j, pj ++ [l])
+        | let res = (l =~ "^[ \t]*([^ \t=]+)[ \t]*=(.*)$"), not $ null res == True = (e ++ [mkEnv $ extractMatches res], j, pj) -- Read environment line
+        | let res = (l =~ "^[ \t]*([[:digit:]]+)[ \t]+([[:digit:]]+)[ \t]+([^ \t/]+)[ \t]+([^ \t].*)$"), not $ null res = (e, j ++ [mkJob $ extractMatches res], pj) -- Read job line
+        | let res = (l =~ "^[ \t]*(@[^ \t]+)[ \t]+([[:digit:]]+)[ \t]+([^ \t/]+)[ \t]+([^ \t].*)$"), not $ null res = (e, j, pj ++ [mkPeriodJob $ extractMatches res]) -- Read period job line
         | otherwise = (e, j, pj)
-        where lineClass = classifyLine l
+    extractMatches = tail . head
 
 main = toTry `catch` handler
 
@@ -42,7 +66,8 @@ toTry = do
     args <- getArgs
     if length args == 0 then do
         contents <- readFile yanacrontab
-        print $ groupTabLines $ mergeTabLines contents
+        let (envs, jobs, periodJobs) = parseTabLines $ mergeTabLines contents
+        mapM_ (forkIO . callCommand . jCommand) jobs
     else return ()
 
 handler :: IOError -> IO ()
